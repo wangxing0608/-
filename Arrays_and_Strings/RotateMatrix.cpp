2a3034c31f533dbbf5d14db3d6caf7cefb990515
wangxing0608 <wangxing0608@gmail.com>
2019-07-03 22:14:26 +0800

    1.8 RotateMatrix.cpp

---
+++ Arrays_and_Strings/RotateMatrix.cpp
@@ -0,0 +1,27 @@
+//
+// Created by wangxing on 19-7-3.
+//
+
+// 1.8 旋转二维矩阵
+// 给定一个NxN矩阵,矩阵每个元素为4bytes,对该执行矩阵旋转90度的操作
+
+
+#include <iostream>
+
+void helper_transpose(int **matrix, int N){
+    for (int i=0;i<N;i++){
+        for (int j=i+1;j<N;++j){
+            if (i!=j) {
+                std::swap(matrix[i][j], matrix[j][i])
+            }
+        }
+    }
+}
+
+void helper_reverse(int *row, int N){
+    for (int i=0;i<N/2;++i){
+        std::swap(row[i], row[N-i-1])
+    }
+}
+
+
